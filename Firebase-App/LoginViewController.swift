dc782f280c32795629325facdc7e1ddb598e558c
Juan C <juanceballos@pursuit.org>
2020-05-05 01:01:35 -0400

    setup ccpods, prog ui

---
+++ Firebase-App/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Firebase-App
+//
+//  Created by Juan Ceballos on 5/5/20.
+//  Copyright © 2020 Juan Ceballos. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+        
+    }
+
+}
