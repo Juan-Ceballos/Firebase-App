9ff9006eca725ada9c971958d764d2b7ceb3a923
Juan C <juanceballos@pursuit.org>
2020-05-05 20:08:17 -0400

    adding users to database

--- Firebase-App/FIrebaseServices/DatabaseService.swift
+++ Firebase-App/FIrebaseServices/DatabaseService.swift
@@ -12,4 +12,29 @@ import FirebaseAuth
 
 class DatabaseService   {
     
+    static let usersCollection = "users"
+    static let postsCollection = "posts"
+    
+    private let db = Firestore.firestore()
+    
+    public func createDatabaseUser(authDataResult: AuthDataResult,
+                                   completion: @escaping (Result<Bool, Error>) -> ())  {
+        guard let email = authDataResult.user.email else {
+            return
+        }
+        db.collection(DatabaseService.usersCollection)
+            .document(authDataResult.user.uid)
+            .setData(["email" : email,
+                      "createdDate": Timestamp(date: Date()),
+                      "userId": authDataResult.user.uid]) { (error) in
+                        
+                        if let error = error {
+                            completion(.failure(error))
+                        } else {
+                            completion(.success(true))
+                        }
+        }
+    }
+    
 }
+
